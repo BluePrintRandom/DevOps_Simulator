444e2a9a36c95423cacade45d2f2f4d7af3664d0
CubeSpawn <cubespawn@gmail.com>
2021-08-22 17:43:23 -0500

    update

--- base/shell/inject.sh
+++ base/shell/inject.sh
@@ -1,10 +1,18 @@
+echo "10.42.0.201"
 ssh 10.42.0.201 "bash -s" <./vercheck.sh
+echo "10.42.0.202"
 ssh 10.42.0.202 "bash -s" <./vercheck.sh
+echo "10.42.0.203"
 ssh 10.42.0.203 "bash -s" <./vercheck.sh
+echo "10.42.0.204"
 ssh 10.42.0.204 "bash -s" <./vercheck.sh
+echo "10.42.0.206"
 ssh 10.42.0.206 "bash -s" <./vercheck.sh
+echo "10.42.0.207"
 ssh 10.42.0.207 "bash -s" <./vercheck.sh
+echo "10.42.0.208"
 ssh 10.42.0.208 "bash -s" <./vercheck.sh
+echo "10.42.0.209"
 ssh 10.42.0.209 "bash -s" <./vercheck.sh
 
 exit 0
