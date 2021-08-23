5d3584b43fd54c9e4df25017fd9136b314a247de
CubeSpawn <cubespawn@gmail.com>
2021-08-22 23:59:56 -0500

    rancher install shell

---
+++ base/shell/rnch-mach.sh
@@ -0,0 +1,34 @@
+echo "10.42.0.201 ********************"
+ssh 10.42.0.201 \
+	docker-machine create -d virtualbox \
+        --virtualbox-boot2docker-url https://releases.rancher.com/os/latest/rancheros.iso \
+        --virtualbox-memory 2048 \
+        vm01t
+
+echo "10.42.0.202 ********************"
+
+ssh 10.42.0.201 \
+	docker-machine create -d virtualbox \
+        --virtualbox-boot2docker-url https://releases.rancher.com/os/latest/rancheros.iso \
+        --virtualbox-memory 2048 \
+        vm02t
+
+echo "10.42.0.203 ********************"
+
+
+echo "10.42.0.204 ********************"
+
+
+echo "10.42.0.206 ********************"
+
+
+echo "10.42.0.207 ********************"
+
+
+echo "10.42.0.208 ********************"
+
+
+echo "10.42.0.209 ********************"
+
+
+
