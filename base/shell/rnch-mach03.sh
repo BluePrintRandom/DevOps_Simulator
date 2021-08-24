ba41e2561e758d8823620473a38968f05fca9806
CubeSpawn <cubespawn@gmail.com>
2021-08-24 17:57:48 -0500

    docker-machine + vboxmanage

--- base/shell/rnch-mach03.sh
+++ base/shell/rnch-mach03.sh
@@ -3,5 +3,7 @@ ssh 10.42.0.203 \
 	docker-machine create -d virtualbox \
         --virtualbox-boot2docker-url https://releases.rancher.com/os/latest/rancheros.iso \
         --virtualbox-memory 3072 \
-	--virtualbox-cpus 2 \
-        vm03
+	vm03 \
+vboxmanage modifyvm --cpus 2 vm03
+
+exit 0
