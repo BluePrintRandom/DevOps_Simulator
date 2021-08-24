dc664b77497614ca3b0cb598d8ae439776aa5042
CubeSpawn <cubespawn@gmail.com>
2021-08-24 18:11:43 -0500

    test concludes

--- base/shell/rnch-mach03.sh
+++
@@ -1,9 +0,0 @@
-echo "10.42.0.203 ********************"
-ssh 10.42.0.203 \
-	docker-machine create -d virtualbox \
-        --virtualbox-boot2docker-url https://releases.rancher.com/os/latest/rancheros.iso \
-        --virtualbox-memory 3072 \
-	vm03 \
-vboxmanage modifyvm --cpus 2 vm03
-
-exit 0
--- base/shell/rnch-mach06.sh
+++
@@ -1,7 +0,0 @@
-echo "10.42.0.206 ********************"
-ssh 10.42.0.206 \
-	docker-machine create -d virtualbox \
-        --virtualbox-boot2docker-url https://releases.rancher.com/os/latest/rancheros.iso \
-        --virtualbox-memory 3072 \
-	--virtualbox-cpus 2 \
-        vm06
