f2eb1c34e388a8288077577c867c7cc0ec6f6e16
CubeSpawn <cubespawn@gmail.com>
2021-08-31 13:02:40 -0500

    added vagrant for base

--- resources/shell/inst_docker.sh
+++ resources/shell/inst_docker.sh
@@ -1,8 +1,8 @@
 #from: https://docs.docker.com/engine/install/ubuntu/
 
-sudo apt-get update
+sudo apt update
 
-sudo apt-get install \
+sudo apt install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -13,7 +13,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 
 echo  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
+sudo apt update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 
--- resources/shell/inst_vagrant.sh
+++ resources/shell/inst_vagrant.sh
@@ -8,6 +8,10 @@ curl -O https://releases.hashicorp.com/vagrant/2.2.18/vagrant_2.2.18_x86_64.deb
 
 sudo apt install -y ./vagrant_2.2.18_x86_64.deb
 
+vagrant plugin install vagrant-vboxmanage
+
+vagrant plugin install vagrant-vbguest
+
 cd ~/
 vagrant --version
 
